7abaed435e910f6916debb4133b96e9988f2b75e
EdisonShaw <122192527+EdisonShaw@users.noreply.github.com>
2024-07-20 15:00:23 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -200,7 +200,7 @@ export default {
 
                     default:
                         // return new Response('Not found', { status: 404 });
-                        url.hostname = '';
+                        url.hostname = '43.207.136.33';
                         url.protocol = 'https:';
                         request = new Request(url, request);
                         return await fetch(request);
